45935205c32223f96d7e33a9afef094bc38bea8b
Ulyana <ulyavava@gmail.com>
2025-06-14 20:25:27 +0500

    исправлен  public/index.php

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use App\Kernel;
 use Symfony\Component\Runtime\Runner\SymfonyApplicationRunner;
 use Symfony\Component\Runtime\GenericRuntime;
 
-require_once __DIR__.'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'] ?? 'prod', (bool) ($context['APP_DEBUG'] ?? false));
